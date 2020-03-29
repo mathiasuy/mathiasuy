5f903f8119ed05c3485f5c54963b93552997493e
Mathias Battistella <mathias2b@gmail.com>
2020-03-29 02:43:57 -0300

    asd

--- index.php
+++ index.php
@@ -16,6 +16,15 @@
 <body>
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="http://www.googletagmanager.com/gtag/js?id=UA-112110069-4"></script>
+
+	<nav>
+		<ul>
+			<li><a href="http://so.mb.uy">Material de Sistemas operativos (FING)</a></li>
+			<li><a href="http://tl.mb.uy">Material de Teoría de Lenguajes (FING)</a></li>
+		 	<li><a href="http://tv.mb.uy">TV en vivo</a></li>
+		</ul>
+	</nav>
+
 	<script>
 	  window.dataLayer = window.dataLayer || [];
 	  function gtag(){dataLayer.push(arguments);}
@@ -38,7 +47,7 @@
 
 
 		echo "<h1>api-petshopp (java)</h1>";
-		
+
 		echo "<br>";
 
 		$json=file_get_contents("http://jtom.mooo.com:8002/pet/get/1");
@@ -50,6 +59,7 @@
 
 		
 	 ?>
+	 
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
