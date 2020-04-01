166ab9915439d7034b2df3e515fe21047538626e
Mathias Battistella <mathias2b@gmail.com>
2020-04-01 01:08:07 -0300

    codersrank widget

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 		}
 
 	</style>
+
 </head>
 <body>
 	<!-- Global site tag (gtag.js) - Google Analytics -->
@@ -37,6 +38,8 @@
 		 	<li><a href="https://jtom.mooo.com">Otro front</a></li>
 		</ul>
 	</nav>
+
+	<codersrank-widget username="mathiasuy"></codersrank-widget>
 
 	<script>
 	  window.dataLayer = window.dataLayer || [];
@@ -73,6 +76,7 @@
 		
 	 ?>
 	 
+  	<script src="https://profile.codersrank.io/widget/widget.js"></script>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
